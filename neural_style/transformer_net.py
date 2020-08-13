626ac77983a1435b623cc88bf02366891706efd7
ramagg <rg1998@hotmail.es>
2020-08-13 00:56:26 -0300

    didnt work

--- neural_style/transformer_net.py
+++ neural_style/transformer_net.py
@@ -41,9 +41,9 @@ class TransformerNet(torch.nn.Module):
         y = self.deconv3(y)
 
         # https://github.com/lengstrom/fast-style-transfer/blob/master/src/transform.py
-        y = self.tan(self.relu(y))*150
+        # y = self.tan(self.relu(y))*150
         # y = self.tan(y)*150
-        # y = self.tan(y)*150 + 255./2
+        y = self.tan(y)*150 + 255./2
         return y
 
 
