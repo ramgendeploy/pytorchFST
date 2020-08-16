8c5f11bfd5f2d88c571af97f1a34f2d7b4338960
ramagg <rg1998@hotmail.es>
2020-08-15 21:45:54 -0300

    without tan

--- neural_style/transformer_net.py
+++ neural_style/transformer_net.py
@@ -43,7 +43,7 @@ class TransformerNet(torch.nn.Module):
         # https://github.com/lengstrom/fast-style-transfer/blob/master/src/transform.py
         # y = self.tan(self.relu(y))*150
         # y = self.tan(y)*150
-        y = self.tan(y)*150 + 255./2
+        # y = self.tan(y)*150 + 255./2
         return y
 
 
